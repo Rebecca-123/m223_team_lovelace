c841d0a8f2339ecad2fce9bb76895791acccf911
Ritvik-Keerthi <89219486+Ritvik-Keerthi@users.noreply.github.com>
2021-09-21 12:24:50 -0700

    algorithms.py gone

--- templates/algorithms.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image, ImageDraw
-
-img = Image.open('/static/assets/solarsystem_stego.jpg')
-
-draw = ImageDraw.Draw(img)
-draw.text((30, 60), "This is a test!", fill=(223,223,223))
-img.show()
-img.save("static/assets/new_solarsystem_stego.jpg")

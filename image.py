68c72f4b57b6e131b008efb4432d508b380d9980
Christina Lee <christinlee367@gmail.com>
2021-09-22 11:16:41 -0700

    Big O Notation finish?

--- image.py
+++ image.py
@@ -55,6 +55,7 @@ def image_data(path="static/assets/", img_list=None):  # path of static images i
         img_dict['binary_array'] = []
         # 'data' is a list of RGB data, the list is traversed and hex and binary lists are calculated and formatted
         for pixel in img_dict['data']:
+            img_dict['gray_data'] = []
             # hexadecimal conversions
             hex_value = hex(pixel[0])[-2:] + hex(pixel[1])[-2:] + hex(pixel[2])[-2:]
             hex_value = hex_value.replace("x", "0")
@@ -63,8 +64,7 @@ def image_data(path="static/assets/", img_list=None):  # path of static images i
             bin_value = bin(pixel[0])[2:].zfill(8) + " " + bin(pixel[1])[2:].zfill(8) + " " + bin(pixel[2])[2:].zfill(8)
             img_dict['binary_array'].append(bin_value)
         # create gray scale of image, ref: https://www.geeksforgeeks.org/convert-a-numpy-array-to-an-image/
-        img_dict['gray_data'] = []
-        for pixel in img_dict['data']:
+        # for pixel in img_dict['data']:
             average = (pixel[0] + pixel[1] + pixel[2]) // 3
             if len(pixel) > 3:
                 img_dict['gray_data'].append((average, average, average, pixel[3]))

b8cfe6480cc028d596ab6ca19c798291701d6ead
Christina Lee <christinlee367@gmail.com>
2021-09-23 11:35:03 -0700

    toggle gone

--- image.py
+++ image.py
@@ -39,7 +39,7 @@ def image_formatter(img, img_type):
 def image_data(path="static/assets/", img_list=None):  # path of static images is defaulted
     if img_list is None:  # color_dict is defined with defaults
         img_list = [
-            {'source': "iconsdb.com", 'label': "Black Hole", 'file': "black_hole.jpg"},
+            {'source': "iconsdb.com", 'label': "Black Hole", 'file': "mars.png"},
         ]
     # gather analysis data and meta data for each image, adding attributes to each row in table
     for img_dict in img_list:

140122155486a8a61f8aafb6dce0888bbce81ca9
Christina Lee <christinlee367@gmail.com>
2021-09-23 11:09:39 -0700

    image_data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from image import image_data
 # create an instance of flask object
 app = Flask(__name__)
 
@@ -137,7 +138,7 @@ def binary_ritvik():
 
 @app.route("/rgblab/")
 def RGB():
-    return render_template("rgb.html")
+    return render_template("rgb.html",image=image_data())
 
 @app.route("/rosetta/")
 def rosetta():

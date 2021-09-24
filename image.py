4828a707ec4d6cdffcb9b3ea768c719ab1e9ceeb
Christina Lee <christinlee367@gmail.com>
2021-09-23 20:26:12 -0700

    o notation

--- image.py
+++ image.py
@@ -39,7 +39,7 @@ def image_formatter(img, img_type):
 def image_data(path="static/assets/", img_list=None):  # path of static images is defaulted
     if img_list is None:  # color_dict is defined with defaults
         img_list = [
-            {'source': "iconsdb.com", 'label': "Black Hole", 'file': "mars.png"},
+            {'source': "iconsdb.com", 'label': "Black Hole", 'file': "comet2.jpg"},
         ]
     # gather analysis data and meta data for each image, adding attributes to each row in table
     for img_dict in img_list:
@@ -56,24 +56,24 @@ def image_data(path="static/assets/", img_list=None):  # path of static images i
         # Numpy is used to allow easy access to data of image, python list
         img_dict['data'] = numpy.array(img_data)
         img_dict['hex_array'] = []
+        img_dict['gray_data'] = []
+        hex_value = hex(pixel[0])[-2:] + hex(pixel[1])[-2:] + hex(pixel[2])[-2:]
+        hex_value = hex_value.replace("x", "0")
+        img_dict['hex_array'].append("#" + hex_value)
+        bin_value = bin(pixel[0])[2:].zfill(8) + " " + bin(pixel[1])[2:].zfill(8) + " " + bin(pixel[2])[2:].zfill(8)
+        img_dict['binary_array'].append(bin_value)
+        average = (pixel[0] + pixel[1] + pixel[2]) // 3
+        if len(pixel) > 3:
+            img_dict['gray_data'].append((average, average, average, pixel[3]))
+        else:
+            img_dict['gray_data'].append((average, average, average))
         img_dict['binary_array'] = []
         # 'data' is a list of RGB data, the list is traversed and hex and binary lists are calculated and formatted
-        for pixel in img_dict['data']:
-            img_dict['gray_data'] = []
+        # for pixel in img_dict['data']:
             # hexadecimal conversions
-            hex_value = hex(pixel[0])[-2:] + hex(pixel[1])[-2:] + hex(pixel[2])[-2:]
-            hex_value = hex_value.replace("x", "0")
-            img_dict['hex_array'].append("#" + hex_value)
             # binary conversions
-            bin_value = bin(pixel[0])[2:].zfill(8) + " " + bin(pixel[1])[2:].zfill(8) + " " + bin(pixel[2])[2:].zfill(8)
-            img_dict['binary_array'].append(bin_value)
         # create gray scale of image, ref: https://www.geeksforgeeks.org/convert-a-numpy-array-to-an-image/
         # for pixel in img_dict['data']:
-            average = (pixel[0] + pixel[1] + pixel[2]) // 3
-            if len(pixel) > 3:
-                img_dict['gray_data'].append((average, average, average, pixel[3]))
-            else:
-                img_dict['gray_data'].append((average, average, average))
         img_reference.putdata(img_dict['gray_data'])
         img_dict['base64_GRAY'] = image_formatter(img_reference, img_dict['format'])
     return img_list  # list is returned with all the attributes for each image dictionary

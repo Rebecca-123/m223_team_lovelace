854889796e59eff7862bd30ab49051cfd94ab8b8
Ritvik-Keerthi <89219486+Ritvik-Keerthi@users.noreply.github.com>
2021-10-08 10:00:22 -0700

    activity 2 because why not!

---
+++ templates/team/Activity2_1.py
@@ -0,0 +1,11 @@
+p1 = { "name":"John", "age":61, "city":"Eugene"}
+p2 = { "name":"Risa", "age":16, "city":"New York"}
+p3 = { "name":"Ryan", "age":16, "city":"Los Angeles"}
+p4 = { "name":"Shekar", "age":16, "city":"San Francisco"}
+# a list of dictionaries
+list_of_people = [p1, p2, p3, p4]
+# write some code to Print List of people one by one
+print(list_of_people, 0)
+print(list_of_people, 1)
+print(list_of_people, 2)
+print(list_of_people, 3)
